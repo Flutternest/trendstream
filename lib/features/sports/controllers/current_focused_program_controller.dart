2039dc295b54b2ee359287e6595c4e4a0a50309f
Abhishek Anand <abhishekanandfn@Abhisheks-MacBook-Pro.local>
2025-09-03 17:18:47 +0530

    fixed sports view not getting focus on second access issue

--- lib/features/sports/controllers/current_focused_program_controller.dart
+++ lib/features/sports/controllers/current_focused_program_controller.dart
@@ -1,6 +1,6 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:latest_movies/features/sports/models/sports_event/sports_event.dart';
 
-final currentFocusedEventController = StateProvider<SportsEvent?>((ref) {
+final currentFocusedEventController = StateProvider.autoDispose<SportsEvent?>((ref) {
   return null;
 });
